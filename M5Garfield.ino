9bd7286427c5a79e9eaf0fa1051ef5ed03405144
duthienkt <blueskythien2010@live.com>
2019-10-06 19:07:24 +0700

    - clean code

--- M5Garfield.ino
+++ M5Garfield.ino
@@ -3,36 +3,6 @@
 #include "math.h"
 #include "tetris.h"
 
-float w = 320;
-float h = 240;
-
-int  blockType = 0;
-int blockState = 0;
-
-void onKey(uint8_t _key)
-{
-  if (!(_key & G_KEY_UP)){
-    blockType = (blockType +1)%7;
-    blockState = 0;
-  }
-  if (!(_key & G_KEY_DOWN))
-  {
-    blockType = (blockType + 6) % 7;
-    blockState = 0;
-  }
-
-  if (!(_key & G_KEY_A))
-  {
-    blockState = (blockState + 1) % 4;
-  }
-
-  if (!(_key & G_KEY_B))
-  {
-    blockState = (blockState + 3) % 4;
-  }
-}
-
-
 void setup()
 {
   gInit();
